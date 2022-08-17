924cc2a0fb1f356d69b10eaf5d50c89c61a09c68
Deji Abiola <adedejiabiola56@gmail.com>
2022-08-17 13:27:54 +0100

    delete question endpoint complete

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -97,6 +97,23 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+    @app.route('/questions/<int:question_id>', methods=["DELETE"])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(
+                Question.id == question_id).one_or_none()
+
+            if question is None:
+                abort(404)
+
+            question.delete()
+
+            return jsonify({
+                "success": True,
+                "deleted": question_id,
+            })
+        except:
+            abort(422)
 
     '''
   @TODO: 
@@ -157,4 +174,14 @@ def create_app(test_config=None):
             404,
         )
 
+    @app.errorhandler(422)
+    def unprocessable_entity(error):
+        return (
+            jsonify({
+                "success": False,
+                "error": 422,
+                "message": "unprocessable entity"
+            }),
+            422,
+        )
     return app
