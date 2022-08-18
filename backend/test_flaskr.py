4674616e38b3dca41b0f9402a6e461b8a5b92fb2
Deji Abiola <adedejiabiola56@gmail.com>
2022-08-18 12:30:29 +0100

    add .env variables

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,7 +14,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
+        self.database_name = os.getenv("DB_NAME", "trivia_test")
         self.database_path = "postgresql://{}/{}".format(
             'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
